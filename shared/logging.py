043a97595d2127c52b42f794a08481214be0cde3
rMaxiQp <zq2@illinois.edu>
2025-05-03 23:16:43 -0500

    logger fix

--- shared/logging.py
+++ shared/logging.py
@@ -1,4 +1,7 @@
 import logging
 
+logging.basicConfig(
+    format="%(asctime)s-%(name)s|%(levelname)s [%(filename)s:%(lineno)d] %(message)s",
+    level=logging.INFO,
+)
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
